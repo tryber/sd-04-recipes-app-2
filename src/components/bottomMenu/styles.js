03fb777d36ec2b3c7db54ca1125be7bd8c5ca318
Josias Viveiro <josiasviveiro@gmail.com>
2020-08-05 17:20:45 -0300

    CSS do BottomMenu - 2

--- src/components/bottomMenu/styles.js
+++ src/components/bottomMenu/styles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 const FooterContainer = styled.footer`
+  background-color: var(--primary);
+  bottom: 0;
   display: flex;
   justify-content: space-between;
   padding: 1% 5%;
-  background-color: var(--primary);
+  position: fixed;
+  width: 100%;
 `;
 
 export default FooterContainer;
