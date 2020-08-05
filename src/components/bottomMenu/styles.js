efe97dc87b84170b6ff70200d90194477ed67071
Josias Viveiro <josiasviveiro@gmail.com>
2020-08-05 17:03:23 -0300

    Css do BottomMenu

---
+++ src/components/bottomMenu/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  padding: 1% 5%;
+  background-color: var(--primary);
+`;
+
+export default FooterContainer;
