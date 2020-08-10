4f9dd63992cd0077df2159391fff68b3eec30682
Josias Viveiro <josiasviveiro@gmail.com>
2020-08-10 16:37:51 -0300

    Exportação da pág. Explorar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from './Login/Login';
 export { default as MainPage } from './MainPage/MainPage';
 export { default as RecipeDetails } from './RecipeDetails/RecipeDetails';
+export { default as Explore } from './Explore/Explore';
