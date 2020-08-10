2bcb375ad8ff30b05f33ea83f0abcd2c55a22b6b
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-08-10 19:39:31 -0300

    pass the continuar test

--- src/pages/RecipeDetails/RecipeDetails.js
+++ src/pages/RecipeDetails/RecipeDetails.js
@@ -23,25 +23,63 @@ const RecipeDetails = ({
   const [drink, setDrink] = useState({});
   const [mealValue, setMealValue] = useState({});
   const [drinkValue, setDrinkValue] = useState({});
-
+  const [isInProgress, setIsInProgress] = useState(false);
   const type = path.includes('comidas') ? 'meal' : 'cocktail';
   const food = path.includes('comidas') ? mealValue : drinkValue;
   const inProgress = path.includes('in-progress');
 
   useEffect(() => {
     getFoodById(type, id).then((resp) => {
+      let key = 'Meal';
       if (type === 'meal') {
         setMeal(resp[0]);
+      } else {
+        key = 'Drink';
+        setDrink(resp[0]);
       }
-      setDrink(resp[0]);
       setLoading(false);
+      if (localStorage.inProgressRecipes) {
+        const foodsInProgress = JSON.parse(localStorage.inProgressRecipes);
+        console.log(foodsInProgress.meals);
+        const foodIsInProgress = Object.keys(foodsInProgress.meals).includes(resp[0][`id${key}`]);
+        setIsInProgress(foodIsInProgress);
+      } else {
+        localStorage.inProgressRecipes = JSON.stringify({ cocktails: {}, meals: {} });
+      }
     });
   }, []);
 
   useEffect(() => {
-    setMealValue({ item: meal, key: 'Meal', path: 'comidas' });
-    setDrinkValue({ item: drink, key: 'Drink', path: 'bebidas' });
+    setMealValue({ item: meal, key: 'Meal', path: 'comidas', type: 'comida' });
+    setDrinkValue({ item: drink, key: 'Drink', path: 'bebidas', type: 'bebida' });
   }, [meal, drink]);
+
+  const addToInProgressRecipes = (foodItem) => {
+    console.log(foodItem);
+    if (isInProgress) return null;
+    if (foodItem.key === 'Meal') {
+      const mealsInProgress = JSON.parse(localStorage.inProgressRecipes);
+      console.log('isMeal');
+      localStorage.inProgressRecipes = JSON.stringify({
+        ...JSON.parse(localStorage.inProgressRecipes),
+        meals: {
+          ...mealsInProgress.meals,
+          [foodItem.item.idMeal]: Object.keys(foodItem.item)
+            .filter((item) => item.includes('strIngredient') && foodItem.item[item])
+            .map((ingredient) => foodItem.item[ingredient]),
+        },
+      });
+    } else {
+      localStorage.inProgressRecipes = JSON.stringify({
+        ...localStorage.inProgressRecipes,
+        cocktails: {
+          ...localStorage.inProgressRecipes.cocktails,
+          [foodItem.item.idDrink]: [foodItem.item],
+        },
+      });
+    }
+    return foodItem.item;
+  };
 
   if (loading) return <Loading />;
 
@@ -68,7 +106,12 @@ const RecipeDetails = ({
           {food.item.strYoutube && <Video path={path} food={food.item} />}
           <Recomendations type={type} />
           <Link to={`/${food.path}/${food.item[`id${food.key}`]}/in-progress`}>
-            <Button.fixed data-testid="start-recipe-btn">Start Recipe</Button.fixed>
+            <Button.fixed
+              data-testid="start-recipe-btn"
+              onClick={() => addToInProgressRecipes(food)}
+            >
+              {isInProgress ? 'Continuar Receita' : 'Iniciar Receita'}
+            </Button.fixed>
           </Link>
         </div>
       )}
