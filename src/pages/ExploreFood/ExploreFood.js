60590dd56fb7a1a9413b5d0c327d1ec90cae39cc
Josias Viveiro <josiasviveiro@gmail.com>
2020-08-10 18:16:13 -0300

    Botão/Link Me Surpreenda! funcionando

--- src/pages/ExploreFood/ExploreFood.js
+++ src/pages/ExploreFood/ExploreFood.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { BottomMenu, Header } from '../../components';
+import { BottomMenu, Header, Loading } from '../../components';
+import { getFoodByRandom } from '../../services/api';
 
 const ExploreFood = ({ match: { path } }) => {
+  const [food, setFood] = useState([]);
+  const [loading, setLoading] = useState(true);
   const page = path.includes('comidas');
   const pathFood = page ? 'comidas' : 'bebidas';
   const type = page ? 'meal' : 'cocktail';
+  const key = page ? 'Meal' : 'Drink';
+
+  useEffect(() => {
+    getFoodByRandom(type).then((resp) => {
+      setFood(resp[0]);
+      setLoading(false);
+    });
+  }, []);
+
+  console.log(food);
+  if(loading) return <Loading />
   return (
     <div>
       <Header pageTitle="Explorar" />
@@ -17,7 +31,7 @@ const ExploreFood = ({ match: { path } }) => {
           Por Local de Origem
         </Link>
       )}
-      <Link to={`/${pathFood}/:id`} data-testid="explore-surprise">
+      <Link to={`/${pathFood}/${food[`id${key}`]}`} data-testid="explore-surprise">
         Me Surpreenda!
       </Link>
       <BottomMenu />
