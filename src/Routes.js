97da2f7e57c19de9d6bc3efd253ca65ad7138fd1
deboracosilveira <deboracosilveira@gmail.com>
2020-08-12 10:13:50 -0300

    Todas outras rotas comentadas

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Login, MainPage, RecipeDetails, DoneRecipes, Explore, ExploreFood } from './pages';
+import { Login/* , MainPage, RecipeDetails, DoneRecipes, Explore, ExploreFood */ } from './pages';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Login} />
-      <Route exact path="/comidas" component={MainPage} />
+      {/* <Route exact path="/comidas" component={MainPage} />
       <Route exact path="/bebidas" component={MainPage} />
       <Route exact path="/comidas/:id" component={RecipeDetails} />
       <Route exact path="/bebidas/:id" component={RecipeDetails} />
@@ -14,13 +14,13 @@ const Routes = () => (
       <Route exact path="/bebidas/:id/in-progress" component={RecipeDetails} />
       <Route exact path="/explorar" component={Explore} />
       <Route exact path="/explorar/comidas" component={ExploreFood} />
-      <Route exact path="/explorar/bebidas" component={ExploreFood} />
+      <Route exact path="/explorar/bebidas" component={ExploreFood} /> */}
       {/* <Route exact path="/explorar/comidas/ingredientes" component={} />
       <Route exact path="/explorar/bebidas/ingredientes" component={} /> */}
       {/* <Route exact path="/explorar/comidas/area" component={} /> */}
       {/* <Route exact path="/perfil" component={} /> */}
 
-      <Route exact path="/receitas-feitas" component={DoneRecipes} />
+      {/* <Route exact path="/receitas-feitas" component={DoneRecipes} /> */}
       {/* <Route exact path="/receitas-favoritas" component={} /> */}
     </Switch>
   </BrowserRouter>
